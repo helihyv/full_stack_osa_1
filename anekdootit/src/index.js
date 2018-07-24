41cd89d1e84f922e1f865c92c03892d59189bc0c
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2018-07-24 14:51:17 +0300

    Tehtävä 1.12
    Tehtävä 1.12 valmis.

---
+++ anekdootit/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      selected: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState({selected : Math.floor (Math.random() * 6 )})
+
+  }
+
+  render () {
+    return (
+      <div>
+        {this.props.anecdotes[this.state.selected]}
+        <br/>
+        <button onClick = {this.handleClick} >
+          next anecdote
+        </button>
+      </div>
+    )
+  }
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes}/>,
+  document.getElementById('root'));
